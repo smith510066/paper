169ce996a6cded14215a995dbf2abe155da8ddc6
unknown <smith510066@yahoo.com>
2012-09-08 09:39:42 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paper::Application.config.secret_token = 'fda83f42af850a120ee452099094bcc04ab7b78d4680ba8dbce09858f9c6faad407bc944ec67bc6dd0529b989a4b72ba75aed1ae14b26f9db7dfaccbc2e306a4'
